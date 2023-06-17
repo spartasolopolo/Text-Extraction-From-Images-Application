61c2e01aeb91d89adc600431975a9db17a16aeb1
Solomon Wilson <93185107+spartasolopolo@users.noreply.github.com>
2023-06-17 03:37:45 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
 
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    SECRET_KEY = 'pianalytix'
+    SECRET_KEY = 'spartsolopolo'
 
     UPLOADS = "/home/username/app/app/static/uploads"
 
